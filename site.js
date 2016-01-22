860a6cd08e1782d195512e6e94159c13d6333555
Neil Ebrey <neil.ebrey@gmail.com>
2016-01-23 00:04:21 +0100

    Complete

--- site.js
+++ site.js
@@ -1,35 +1,57 @@
 var breakTime = 3;
+var breakStart = false;
 var sessionTime = 25;
+
+var clockStarted;
 
 var seconds = 60;
 var startClock = false;
 
+var wav = 'audio/alarm.ogg';
+var audio = new Audio(wav);
+var played = false;
 
-
+var initialTimePercent = 25 * 60;
+console.log(initialTimePercent)
+var currentPercent;
 
 $(document).ready(function() {
+
+
+
+
+
+
   $('#breakPlus').click(function() {
+
     if (breakTime > 29) {
       return;
     }
+    breakTime = parseInt($('#breakLength').html());
     breakTime++;
     $('#breakLength').text(updateDisplay(breakTime));
   });
 
   $('#breakMinus').click(function() {
+
     if (breakTime < 2) {
       return;
     }
+    breakTime = parseInt($('#breakLength').html());
     breakTime--;
     $('#breakLength').text(updateDisplay(breakTime));
   });
 
   $('#sessionPlus').click(function() {
-    seconds = 60;
+
     if (sessionTime > 29) {
       return;
     }
+    sessionTime = parseInt($('#sessionTime').html());
+    seconds = 60;
     sessionTime++;
+    initialTimePercent = sessionTime * 60;
+    console.log(initialTimePercent)
     $('#sessionLength').text(updateDisplay(sessionTime));
     $('#sessionTime').text(updateDisplay(sessionTime));
     $('#m').text(updateDisplay(sessionTime));
@@ -37,11 +59,16 @@ $(document).ready(function() {
   });
 
   $('#sessionMinus').click(function() {
-    seconds = 60;
+
     if (sessionTime < 2) {
       return;
     }
+    sessionTime = parseInt($('#sessionTime').html());
+
+    seconds = 60;
     sessionTime--;
+    initialTimePercent = sessionTime * 60;
+    console.log(initialTimePercent)
     $('#sessionLength').text(updateDisplay(sessionTime));
     $('#sessionTime').text(updateDisplay(sessionTime));
     $('#m').text(updateDisplay(sessionTime));
@@ -50,13 +77,21 @@ $(document).ready(function() {
 
 
   $('#start').click(function() {
+    $('#circle').circleProgress({
+      animation: false
+    });
+
     if (startClock != false) {
-      clockStarted = false;
+      //clockStarted = false;
       $('#start').removeClass('fa-pause').addClass('fa-play');
+
+
       clearInterval(startClock)
       startClock = false;
     } else {
       $('#start').removeClass('fa-play').addClass('fa-pause');
+      //initialTimePercent = sessionTime * 60;
+      //console.log(initialTimePercent)
       startClock = setInterval(function() {
         calcSeconds()
       }, 100);
@@ -64,29 +99,80 @@ $(document).ready(function() {
     }
   })
 
+  resetProgress();
+
+
+})
+
+function resetProgress() {
+  breakStart = false;
   $('#circle').circleProgress({
     reverse: false,
     value: 1,
     startAngle: 4.7,
+    /*animation: {
+      duration: 1200,
+      easing: "circleProgressEase"
+    },*/
+    animationStartValue: 0,
     size: 300,
     fill: {
-      gradient: ["red", "orange"]
+      gradient: ["red", "orange", "red"]
     }
   });
+}
 
-})
+function progressBreakTime() {
+  $('#circle').circleProgress({
+    fill: {
+      gradient: ["lightgreen", "darkgreen", "lightgreen"]
+    }
+  });
+}
+
+var playsound = function() {
+  if (played) {
+    audio.pause();
+    played = false;
+    return
+  } else {
+
+    audio.play();
+    played = true;
+    setTimeout(playsound, 2000); // check again in a second
+  }
+}
+
+
 
 function calcSeconds() {
-  var secs = sessionTime * 60;
   if (sessionTime == 0 && seconds == 0) {
     sessionTime = 0;
-    /*alert('done!');*/
-    clearInterval(startClock);
+
+    $('#circle').circleProgress({
+      value: 0
+    });
     $('#s').text(updateDisplay(0));
     $('#m').text(updateDisplay(0));
+
+    if (breakTime > 0) {
+      playsound();
+      sessionTime = breakTime;
+      seconds = 60;
+      breakTime = 0;
+      initialTimePercent = sessionTime * 60;
+      progressBreakTime();
+      breakstart = true;
+      return;
+    }
     $('#start').removeClass('fa-pause').addClass('fa-play');
+    playsound();
+    clearInterval(startClock);
+    sessionTime = parseInt($('#sessionTime').html());
+    resetProgress();
     return;
   }
+
   console.log('hi')
   if (seconds == 0) {
     seconds = 60;
@@ -96,12 +182,20 @@ function calcSeconds() {
     seconds = 59;
   }
 
-  $('#circle').circleProgress({
-    value: 1.1
-  });
+
   $('#s').text(updateDisplay(seconds))
   $('#m').text(updateDisplay(sessionTime))
   seconds--;
+  currentPercent = (sessionTime * 60 + seconds + 1) / initialTimePercent;
+  /*console.log("Session Time: " + sessionTime)
+  console.log("Seconds: " + seconds);
+  console.log("Time Percent " + initialTimePercent);
+  console.log("Current Percent " + currentPercent);
+  console.log("---------")*/
+  //console.log(currentPercent)
+  $('#circle').circleProgress({
+    value: currentPercent
+  });
 }
 
 function updateDisplay(val) {
